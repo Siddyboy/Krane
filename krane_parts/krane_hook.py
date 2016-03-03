56abb5e0fd534c81a0b6f9b482cf5e3e93f0e783
Simon Kingsley <scjk@btinternet.com>
2016-03-03 22:59:41 +0000

    Sorted indenting in krane_hook.
    Code works again. Still needs control sorting out.

--- krane_parts/krane_hook.py
+++ krane_parts/krane_hook.py
@@ -7,25 +7,25 @@ class KraneHook(object):
     def stop(self):
         '''Stops the hook dead.'''
 	
-	wiringpi.pwmWrite(18, 0)
-	print("STOP DEAD!")
+        wiringpi.pwmWrite(18, 0)
+        print("STOP DEAD!")
 
-#            hook_speed = 0
+#       hook_speed = 0
 #	    return hook_speed
 #            
     def up_slow(self, hook_speed):
         '''Moves the hook up slowly.'''
 	
-	print("Going up slow...")    
+        print("Going up slow...")    
         wiringpi.digitalWrite(17, 0)
         wiringpi.pwmWrite(18, 500)            
             
     def down_slow(self, hook_speed):
-	'''Moves the hook down slowly.'''
+        '''Moves the hook down slowly.'''
 
-	print("Going down slow...")
-	wiringpi.digitalWrite(17, 1)
-	wiringpi.pwmWrite(18, 500)
+        print("Going down slow...")
+        wiringpi.digitalWrite(17, 1)
+        wiringpi.pwmWrite(18, 500)
 
 
 
